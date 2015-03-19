91ffbc721ceb820cc144e159a6275bf9f076676b
unknown <D:\Data\The Bat!>
2015-03-19 14:27:33 +0300

    Insignificant changes

--- FileSize.php
+++ FileSize.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * FileSize snippet for MODX Evolution
- *
- * Allows to display size of a file placed on server with auto unit select.
+ * FileSize snippet for MODX Evolution.
  *
  * Example usage: [[FileSize? &path=`assets/files/myfile.zip`]]
+ *
+ * The file path must be relative to webroot.
  */
+
 $units = array(
     'gb' => array('size' => 1073741824, 'label' => 'GB'),
     'mb' => array('size' => 1048576,    'label' => 'MB'),
@@ -27,11 +28,14 @@ if ($size > 0) {
             }
         }
     }
-    if ($unit != 'b')
+    if ($unit != 'b') {
         $size = $size / $units[$unit]['size'];
+    }
 }
 else {
-    if ($unit === false) $unit = 'b';
+    if ($unit === false) {
+        $unit = 'b';
+    }
 }
 
 return round($size, 2) . ' ' . $units[$unit]['label'];
